8d4f6ec526cd8c7239901166f7881f9b5c2bae1b
Logan Toler <tolerl1@nku.edu>
2018-09-17 16:01:38 -0400

    updates

--- init-net.sh
+++ init-net.sh
@@ -17,4 +17,9 @@ if [ "$HOSTNAME" =  s1$t$nku ]
 	elif [ "$HOSTNAME" =  c4$t$nku ]
         then echo "IPADDR=10.2.7.228" >> $netconfig
 fi
+sed -i 's~BOOTPROTO=dhcp~BOOTPROTO=static~g' $netconfig
+echo "PREFIX=23" >> $netconfig
+echo "GATEWAY=10.2.6.1" >> $netconfig
+echo "DNS1=10.14.1.10" >> $netconfig
+echo "DNS2=10.11.0.51" >> $netconfig
 #systemctl restart network
